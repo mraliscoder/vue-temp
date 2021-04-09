8a29782789ca34e0cc3fa7960a069a0ebaaa8e0a
Edward <mralis@dpud.net>
2021-04-09 17:05:38 +0300

    Lesson 6. Editing text

---
+++ app/src/dom-helper.js
@@ -0,0 +1,44 @@
+module.exports = class DOMHelper {
+    static parseStringToDom(string) {
+        const parser = new DOMParser();
+        return parser.parseFromString(string, "application/xml");
+    }
+
+    static serializeDomToString(dom) {
+        const serializer = new XMLSerializer();
+        return serializer.serializeToString(dom);
+    }
+
+    static unwrapTextNodes(dom) {
+        dom.body.querySelectorAll("editor-element").forEach((element) => {
+            element.parentNode.replaceChild(element.firstChild, element);
+        })
+    }
+
+    static wrapTextNodes(dom) {
+        const body = dom.body;
+        let textNodes = [];
+
+        function recursy(element) {
+            element.childNodes.forEach((node) => {
+                if (node.nodeName === "#text" && node.nodeValue.replace(/\s+/g, "").length > 0) {
+                    textNodes.push(node);
+                } else {
+                    recursy(node);
+                }
+            });
+        }
+        recursy(body);
+
+        textNodes.forEach((node, i) => {
+            const wrapper = dom.createElement("editor-element");
+
+            node.parentNode.replaceChild(wrapper, node);
+            wrapper.appendChild(node);
+            wrapper.contentEditable = "true";
+            wrapper.setAttribute("nodeid", i);
+        });
+
+        return dom;
+    }
+}
